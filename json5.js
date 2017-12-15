20a932b1bbd47ee1900c9d4c28538a4bc5f18b28
hongmaoxiao <769501294@qq.com>
2017-12-16 00:01:03 +0800

    part parse

--- json5.js
+++ json5.js
@@ -1,16 +1,207 @@
 // json5.js
 // Modern JSON. See README.md for details.
+//
+// This file is modeled directly off of Douglas Crockford's json_parse.js:
+// https://github.com/douglascrockford/JSON-js/blob/master/json_parse.js
+exports.parse = (function() {
+    "use strict";
 
-// TODO Support a reviver argument like the native JSON.parse().
-// https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/JSON/parse
-exports.parse = function (str) {
-    // TODO Actually implement! XXX FIXME For now, stubbing via eval().
-    // Works since JSON5 is a subset of ES5.
-    // Remember to wrap in parentheses since {...} can be ambiguous.
-    return eval('(' + str + ')');
-};
+    // This is a function that can parse a JSON text, producing a JavaScript
+    // data structure. It is a simple, recursive descent parser. It does not use
+    // eval or regular expressions, so it can be used as a model for implementing
+    // a JSON parser in other languages.
 
-exports.stringify = function (obj, replacer, space) {
+    // We are defining the function inside of another function to avoid creating
+    // global variables.
+
+    var at, // the index of the current character
+        ch, // The current character
+        escapee = {
+            '"': '"',
+            '\\': '\\',
+            '/': '/',
+            b: '\b',
+            f: '\f',
+            n: '\n',
+            r: '\r',
+            t: '\t'
+        },
+        text,
+
+        error = function(m) {
+            // Call error when something is wrong.
+            throw {
+                name: 'SyntaxError',
+                message: m,
+                at: at,
+                text: text
+            };
+        },
+
+        next = function(c) {
+            // If a c parameter is provided, verify that it matches the current character.
+            if (c && c !== ch) {
+                error("Expected '" + c + "' instead of '" + ch + "'");
+            }
+            // Get the next character. When there are no more characters,
+            // return the empty string.
+            ch = text.charAt(at);
+            at += 1;
+            return ch;
+        },
+
+        number = function() {
+            // Parse a number value.
+            var number,
+                string = '';
+
+            if (ch === '-') {
+                string = '-';
+                next('-');
+            }
+            while (ch >= '0' && ch <= '9') {
+                string += ch;
+                next();
+            }
+            if (ch === '.') {
+                string += '.';
+                while (next() && ch >= '0' && ch <= '9') {
+                    string += ch;
+                }
+            }
+            if (ch === 'e' || ch === 'E') {
+                string += ch;
+                next();
+                if (ch === '-' || ch === '+') {
+                    string += ch;
+                    next();
+                }
+                while (ch >= '0' && ch <= '9') {
+                    string += ch;
+                    next();
+                }
+            }
+            number = +string;
+            if (!isFinite(number)) {
+                error('Bad number');
+            } else {
+                return number;
+            }
+        },
+
+        string = function() {
+            // Parse a string value.
+            var hex,
+                i,
+                string = '',
+                uffff;
+
+            // When parsing for string values, we must look for " and \ characters.
+            if (ch === '"') {
+                while (next()) {
+                    if (ch === '"') {
+                        next();
+                        return string;
+                    } else if (ch === '\\') {
+                        next();
+                        if (ch === 'u') {
+                            uffff = 0;
+                            for (i = 0; i < 4; i++) {
+                                hex = parseInt(next(), 16);
+                                if (!isFinite(hex)) {
+                                    break;
+                                }
+                                uffff = uffff * 16 + hex;
+                            }
+                            string += String.fromCharCode(uffff);
+                        } else if (typeof escape[ch] === 'string') {
+                            string += escape[ch];
+                        } else {
+                            break;
+                        }
+                    } else {
+                        string += ch;
+                    }
+                }
+            }
+            error('Bad string');
+        },
+
+    white = function() {
+// Skip whitespace.
+      while (ch && ch <= ' ') {
+        next();
+      }
+    },
+
+    word = function() {
+// true, false, or null.
+      switch(ch) {
+        case 't':
+          next('t');
+          next('r');
+          next('u');
+          next('e');
+          return true;
+        case 'f':
+          next('f');
+          next('a');
+          next('l');
+          next('s');
+          next('e');
+          return false;
+        case 'n':
+          next('n');
+          next('u');
+          next('l');
+          next('l');
+          return null;
+      }
+      error("Unexpected '" + ch + "'");
+    },
+
+    value, // Place holder for the value function.
+
+    array = function() {
+// Parse an array value.
+      var array = [];
+
+      if (ch === '[') {
+        next('[');
+        white();
+        if (ch === ']') {
+          next(']');
+          return array; // empty array
+        }
+        while(ch) {
+          array.push(value());
+          white();
+          if (ch === ']') {
+            next(']');
+            return array;
+          }
+          next(',');
+          white();
+        }
+      }
+      error('Bad array');
+    },
+
+    object = function() {
+// Parse an object value.
+      var key,
+        object = {};
+      if (ch === '{') {
+        next('{');
+        white();
+        if (ch === '}') {
+          next('}');
+          return object; // empty object
+        }
+      }
+    }
+});
+exports.stringify = function(obj, replacer, space) {
     // Since regular JSON is a strict subset of JSON5, we'll always output as
     // regular JSON to foster better interoperability. TODO Should we not?
     return JSON.stringify.apply(JSON, arguments);
